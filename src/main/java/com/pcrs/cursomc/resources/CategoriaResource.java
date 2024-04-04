47a14cd18931685ed7d50f2fd80142e4fecc0a73
Paulo Cezar Rodrigues da Silva <conhecer.ti@gmail.com>
2024-04-04 15:51:52 -0400

    Criando Repository e Service para Categoria

--- src/main/java/com/pcrs/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/pcrs/cursomc/resources/CategoriaResource.java
@@ -1,7 +1,12 @@
 package com.pcrs.cursomc.resources;
 
 import com.pcrs.cursomc.domain.Categoria;
+import com.pcrs.cursomc.repositories.CategoriaRepository;
+import com.pcrs.cursomc.services.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +16,9 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
+
+    @Autowired
+    private CategoriaService service;
 
     @GetMapping
     public List<Categoria> listar(){
@@ -25,5 +33,11 @@ public class CategoriaResource {
         return categorias;
     }
 
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<?> find(@PathVariable Integer id) {
+       Categoria obj = service.find(id);
+       return ResponseEntity.ok().body(obj);
+
+    }
 
 }
---
+++ src/main/java/com/pcrs/cursomc/services/CategoriaService.java
@@ -0,0 +1,20 @@
+package com.pcrs.cursomc.services;
+
+import com.pcrs.cursomc.domain.Categoria;
+import com.pcrs.cursomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository repo;
+
+    public Categoria find(Integer id) {
+        Optional<Categoria> obj = repo.findById(id);
+        return obj.orElse(null);
+    }
+}
